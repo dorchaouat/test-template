6113552b3835c9dba1f71402313e68ecfbac82a4
Dor Chaouat <dorch@wix.com>
2023-03-29 17:05:32 +0300

    wip

--- react/src/components/first-widget/index.ts
+++ react/src/components/first-widget/index.ts
@@ -5,4 +5,4 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 const MyComp = reactToWebComponent(WeatherComponent, React, ReactDOM);
 
-customElements.define("weather-component", MyComp);
+customElements.define("first-widget", MyComp);
